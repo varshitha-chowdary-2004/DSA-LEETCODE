2fd7ee09a68f14cb00ffff513fff7966ba90ce60
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-06-13 06:10:57 +0530

    Time: 2 ms (97.02%) | Memory: 45.7 MB (84.10%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -1,23 +1,24 @@
 class Solution {
-    public TreeNode constructBST(List<Integer> arr,int lo,int hi){
+    public TreeNode constructBST(List<TreeNode> arr,int lo,int hi){
         if(lo>hi) return null;
         int mid = lo + (hi-lo)/2;
-        TreeNode root =  new TreeNode(arr.get(mid));
+        TreeNode root = arr.get(mid);
         root.left = constructBST(arr,lo,mid-1);
         root.right = constructBST(arr,mid+1,hi);
         return root;
     }
-    public void inOrder(TreeNode root,List<Integer> arr){
+    public void inOrder(TreeNode root,List<TreeNode> arr){
         if(root==null) return;
         inOrder(root.left,arr);
-        arr.add(root.val);
+        arr.add(root);
         inOrder(root.right,arr);
     }
     public TreeNode balanceBST(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
+        List<TreeNode> arr = new ArrayList<>();
         inOrder(root,arr);
         int n = arr.size();
         return constructBST(arr,0,n-1);
+        
 
         
     }
