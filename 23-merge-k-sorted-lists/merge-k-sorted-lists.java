daac4f24f6db1a02719d9b9d7ee9ee98978ceea9
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-06-22 15:27:07 +0530

    Time: 4 ms (76.62%) | Memory: 44.4 MB (79.01%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,12 +1,9 @@
 class Solution {
     public class Triplet implements Comparable<Triplet>{
         ListNode node;
-        int row;
-        int col;
-        Triplet(ListNode node,int row,int col){
+        
+        Triplet(ListNode node){
             this.node  = node;
-            this.row = row;
-            this.col = col;
         }
         public int compareTo(Triplet t){
             return this.node.val - t.node.val; 
@@ -16,13 +13,13 @@ class Solution {
         if(arr.length==0) return null;
         PriorityQueue<Triplet> pq = new PriorityQueue<>();
         for(int i=0;i<arr.length;i++){
-            if(arr[i]!=null) pq.add(new Triplet(arr[i],i,0));
+            if(arr[i]!=null) pq.add(new Triplet(arr[i]));
         }
         ListNode head = new ListNode(0);
         ListNode temp = head;
         while(pq.size()>0){
             Triplet t = pq.remove();
-            if(t.node.next!=null) pq.add(new Triplet(t.node.next,t.row,t.col+1));
+            if(t.node.next!=null) pq.add(new Triplet(t.node.next));
             temp.next = t.node;
             temp = temp.next;
         }
