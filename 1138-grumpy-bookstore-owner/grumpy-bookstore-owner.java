b592ad97134da6165ed1aadd228393b7dd4620a9
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-06-24 15:32:17 +0530

    Time: 2 ms (99.63%) | Memory: 47.6 MB (64.19%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -1,26 +1,18 @@
 class Solution {
     public int maxSatisfied(int[] arr, int[] grumpy, int k) {
-        int i=0,j=0,maxun = 0,un =0,a=0,b=0,n = arr.length;
-        for(j=0;j<k;j++){
-            if(grumpy[j]==1) un+=arr[j];
+        int i=0,j=k-1,maxun = 0,un =0,a=0,b=0,n = arr.length;
+        for(int x=i;x<=j;x++){
+            if(grumpy[x]==1) un+=arr[x];
         }
-        j--;
-        if(maxun<un){
+        while(j<n){
+            if(maxun<un){
             maxun = un;
             a = i;
             b = j;
-        }
-        i++;j++;
-        while(j<n){
-            //un = un - arr[i-1] + arr[j];
-            if(grumpy[i-1]==1) un-=arr[i-1];
-            if(grumpy[j] ==1) un+=arr[j];
-            if(maxun<un){
-                maxun = un;
-                a = i;
-                b = j;
             }
             i++;j++;
+            if(j<n && grumpy[j] ==1) un+=arr[j];
+            if(grumpy[i-1]==1) un-=arr[i-1]; 
         }
         for(int c = a;c<=b;c++){
             grumpy[c] = 0;
