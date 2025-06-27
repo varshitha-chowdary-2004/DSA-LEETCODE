5b545d6d9ee177826215c5434733f73566613bd4
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-06-27 09:41:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (63.58%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,14 +1,12 @@
+//Tabulation method
 class Solution {
-    public int minCost(int[] cost,int idx,Integer n,int[] dp) {
-        if(idx==n-1 || idx==n-2) return cost[idx];
-        if(dp[idx]!=-1) return dp[idx];
-        return dp[idx] = cost[idx] + Math.min(minCost(cost,idx+1,n,dp),minCost(cost,idx+2,n,dp));
-
-    }
     public int minCostClimbingStairs(int[] cost) {
-        int n=cost.length;
+        int n = cost.length;
         int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return Math.min(minCost(cost,0,n,dp),minCost(cost,1,n,dp));
+        dp[0]=cost[0]; dp[1]= cost[1];
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-1],dp[n-2]);
     }
 }
