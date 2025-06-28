5dff4a20c2ace86c88d654885824ad09c59c2654
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-06-28 19:26:30 +0530

    Time: 80 ms (31.65%) | Memory: 57.6 MB (25.88%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean subsetSum(int i,int[] arr,int k,int[][] dp){
+    /*public boolean subsetSum(int i,int[] arr,int k,int[][] dp){
         if(i<0) {
             if(k==0) return true;
             else return false;
@@ -15,7 +15,7 @@ class Solution {
         ans = skip||pick;
         dp[i][k] = (ans)?1:0;
         return ans;
-    }
+    }*/
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum=0;
@@ -24,12 +24,21 @@ class Solution {
         }
         if(sum%2==1) return false;
         int[][] dp = new int[nums.length][sum/2 +1];
+        
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<sum/2+1;j++){
-                dp[i][j] =-1;
+                
+                boolean skip = (i>0)?(dp[i-1][j]==1):(j==0);
+                if(nums[i]>j) {
+                    dp[i][j] = (skip)?1:0;
+                }else{
+                    boolean pick = (i>0)?(dp[i-1][j-nums[i]]==1):(j==0);
+                    
+                    dp[i][j] = (skip||pick)?1:0;
+                }
+                
             }
         }
-        return subsetSum(n-1,nums,sum/2,dp);
-        
+        return dp[n-1][sum/2]==1;
     }
 }
