8022065c4736943e7cb2e3846d58a130eef7f994
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-07-09 10:11:48 +0530

    Time: 6 ms (11.53%) | Memory: 45.2 MB (46.09%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,16 +1,4 @@
 class Solution {
-    public int minSteps(int i,int j,StringBuilder a,StringBuilder b,int[][] dp){
-        if(i==-1) return j+1;
-        if(j==-1) return i+1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(a.charAt(i)== b.charAt(j)) return dp[i][j] = minSteps(i-1,j-1,a,b,dp);
-        else {
-            int del = minSteps(i-1,j,a,b,dp);
-            int ins = minSteps(i,j-1,a,b,dp);
-            int rep = minSteps(i-1,j-1,a,b,dp);
-            return dp[i][j] = 1+Math.min(del,Math.min(ins,rep));
-        }
-    }
     public int minDistance(String word1, String word2) {
         StringBuilder a = new StringBuilder(word1);
         StringBuilder b = new StringBuilder(word2);
