9c5ca56daa43082f64a8af3ebdd71fdd2e09f4c1
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-07-09 10:11:18 +0530

    Time: 6 ms (11.53%) | Memory: 45.3 MB (30.38%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -15,12 +15,22 @@ class Solution {
         StringBuilder a = new StringBuilder(word1);
         StringBuilder b = new StringBuilder(word2);
         int m = a.length(), n = b.length();
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j] =-1;
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 0; i <= m; i++) dp[i][0] = i;
+        for (int j = 0; j <= n; j++) dp[0][j] = j;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(a.charAt(i-1)== b.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                    int del =  dp[i-1][j];
+                    int ins =  dp[i][j-1] ;
+                    int rep =  dp[i-1][j-1];
+                    dp[i][j] = 1+ Math.min(del,Math.min(ins,rep));
+                }
             }
         }
-        return minSteps(m-1,n-1,a,b,dp);
+        return dp[m][n];
     }
 }
