c80b0e1e1833394655a494a00b6513b05b44ce8b
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-07-30 00:06:24 +0530

    Time: 3 ms (93.89%) | Memory: 52.7 MB (5.64%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -5,14 +5,14 @@ class Solution {
         int l=0,r=0;
         while(r<nums.length){
             if(nums[r]==0) zeroes++;
+            if(zeroes>k){
+                if(nums[l]==0) zeroes--;
+                l++;
+            }     
             if(zeroes<=k){
                 int  len = r-l+1;
                 max = Math.max(max,len);
-            }else{
-                while(nums[l]!=0) l++;
-                l++;
-                zeroes--;
-            }
+            }     
             r++;
         }
         return max;
