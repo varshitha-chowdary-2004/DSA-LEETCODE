09d14871367b62f975d32d1cb8d9aea586caa674
Alapati Varshitha <varshithachowdary21@gmail.com>
2025-08-29 21:15:11 +0530

    Time: 59 ms (5.91%) | Memory: 55.8 MB (15.94%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,25 +1,22 @@
 class Solution {
-    public int totalFruit(int[] arr) {
-        HashMap<Integer,Integer> map= new HashMap<>();
-        int i=0,j=0,maxlen =0,n=arr.length;
-        while(j<n){
-            int ele = arr[j];
-            if(map.containsKey(ele)){
-                map.put(ele,map.get(ele)+1);
-            }else map.put(ele,1);
-            
-            if(map.size()>2){
-                int freq = map.get(arr[i]);
-                if(freq==1) map.remove(arr[i]);
-                else map.put(arr[i],freq-1);
-                i++;
+    public int totalFruit(int[] fruits) {
+        int maxfruits = 0;
+        int l=0,r=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        while(r<fruits.length){
+            if(map.containsKey(fruits[r])){
+                map.put(fruits[r],map.get(fruits[r])+1);
+            }else{
+                map.put(fruits[r],1);
             }
-
-            int len = j-i+1;
-            maxlen = Math.max(len,maxlen);
-            j++;
+            if(map.size()>2){
+                map.put(fruits[l],map.get(fruits[l])-1);
+                if(map.get(fruits[l])==0) map.remove(fruits[l]);
+                l++;
+            }
+            if(map.size()<=2) maxfruits = Math.max(maxfruits,r-l+1);
+            r++;
         }
-        
-        return maxlen;
+        return maxfruits;
     }
 }
